0e52f6a1e06d01e3edea4ffe5a34f32809d81bfd
Yauhen <penkin.evgeniy@gmail.com>
2017-05-29 11:03:44 +0300

    fix (project): changes for import project

---
+++ index.ts
@@ -0,0 +1,10 @@
+export { cacheDecorator } from './src/cache.decorator';
+export { checkAccess } from './src/checkAccess.decorator';
+export { debounce } from './src/debounce.decorator';
+export { throttle } from './src/throttle.decorator';
+export { timingDecorator } from './src/timing.decorator';
+export { log } from './src/log.decorator';
+
+
+
+
