2b62d4738a9e1a896a9d395e2dc8179d0a452fac
Yauhen <penkin.evgeniy@gmail.com>
2017-05-31 18:14:41 +0300

    fix (timing decorator): now it could work with promises

--- src/timing.decorator.ts
+++ src/timing.decorator.ts
@@ -1,13 +1,23 @@
 export function timingDecorator(target: any, key: any, desc: any) {
   return {
     value: function (...args: any[]) {
-
       const start = performance.now();
       const result = desc.value.apply(this, args);
-      const end = performance.now();
       
-      console.log('Time of running function: ' + (end - start) + ' | Result: ' + result);
-      return result;
+      if(result.then) {
+        return result.then(data => {
+          logTime(start, data);
+          return data;
+        });
+      } else {
+        logTime(start, result);
+        return result;
+      }
     }
   }
+}
+
+function logTime(start: number, result: any) {
+  const end = performance.now();
+  console.log('Time of running function: ' + (end - start) + ' | Result: ' + result);
 }
